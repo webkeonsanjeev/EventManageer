cc2cf349116f15b8e50b338e70db7f2b42c7003c
webkeonsanjeev <webkeon.sanjeev@gmail.com>
2021-04-26 21:26:32 +0530

    Add files via upload

---
+++ Ui/Component/Listing/Columns/EventActions.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Webkul Software.
+ *
+ * @category  Webkul
+ * @package   Webkul_EventManager
+ * @author    Webkul
+ * @copyright Copyright (c) Webkul Software Private Limited (https://webkul.com)
+ * @license   https://store.webkul.com/license.html
+ */
+namespace Webkul\EventManager\Ui\Component\Listing\Columns;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\UrlInterface;
+
+/**
+ * EventActions class is used to change render data
+ */
+class EventActions extends Column
+{
+    const EVENT_URL_PATH_EDIT = 'eventmanager/event/addevent';
+    const EVENT_URL_PATH_DELETE = 'eventmanager/event/delete';
+ 
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+ 
+    /**
+     * @var string
+     */
+    private $editUrl;
+ 
+    /**
+     * @param ContextInterface   $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface       $urlBuilder
+     * @param array              $components
+     * @param array              $data
+     * @param string             $editUrl
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = [],
+        $editUrl = self::EVENT_URL_PATH_EDIT
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        $this->editUrl = $editUrl;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+ 
+    /**
+     * Prepare Data Source
+     *
+     * @param  array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $name = $this->getData('name');
+                if (isset($item['event_id'])) {
+                    $item[$name]['edit'] = [
+                        'href' => $this->urlBuilder->getUrl($this->editUrl, ['event_id' => $item['event_id']]),
+                        'label' => __('Edit')
+                    ];
+                    $urlPath = self::EVENT_URL_PATH_DELETE;
+                    $item[$name]['delete'] = [
+                        'href' => $this->urlBuilder->getUrl($urlPath, ['event_id' => $item['event_id']]),
+                        'label' => __('Delete'),
+                        'confirm' => [
+                            'title' => __('Delete "${ $.$data.event_name}"'),
+                            'message' => __('Are you sure you want to delete a "${ $.$data.event_name }" record?')
+                        ]
+                    ];
+                }
+            }
+        }
+        return $dataSource;
+    }
+}
---
+++ Ui/Component/Listing/Columns/EventData.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Webkul Software.
+ *
+ * @category  Webkul
+ * @package   Webkul_EventManager
+ * @author    Webkul
+ * @copyright Copyright (c) Webkul Software Private Limited (https://webkul.com)
+ * @license   https://store.webkul.com/license.html
+ */
+
+namespace Webkul\EventManager\Ui\Component\Listing\Columns;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * EventData Data Source Class
+ */
+class EventData extends Column
+{
+    /**
+     * Prepare Data Source here create link in grid for product edit.
+     *
+     * @param array $dataSource
+     *
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            $fieldName = $this->getData('name');
+            foreach ($dataSource['data']['items'] as &$item) {
+                if (isset($item['event_content'])) {
+                    $item[$fieldName] = strip_tags($item['event_content']);
+                }
+            }
+        }
+        return $dataSource;
+    }
+}
